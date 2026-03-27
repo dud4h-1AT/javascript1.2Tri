2761bdeefdfdfb91f728f13e0a418659772ea3ea
dud4h-1AT <m.epifanio@escola.pr.gov.br>
2026-03-27 07:58:09 -0300

    update

---
+++ aulas/27032026.js
@@ -0,0 +1,9 @@
+ // Criar Array
+
+ const fila = [1,2,3,4,5,6];
+
+ const filaStringAmigos = ["Isadora", "Vitória", "Apollo"]
+
+ function leramigos(filaStringAmigos){
+    console.log('Você tem ${filaStringAmigos.length'} amigos)
+ }
